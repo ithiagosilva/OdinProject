e15d9e25cf51e0bacfa9e11c4239a37dff6102bb
Thiago Silva <59424663+ithiagosilva@users.noreply.github.com>
2024-12-19 22:19:39 -0300

    Consertando bugs e adicionando melhorias ao projeto biblioteca

--- 7.LibraryAPP/index.js
+++ 7.LibraryAPP/index.js
@@ -12,25 +12,23 @@ let btnAdicionar = document.getElementById('btnAdicionar')
 let btnFechar = document.getElementById('btnFechar')
 let livros = document.getElementById('livros')
 let containerAddLivro = document.querySelector('.container-addLivro')
+let pVazio = document.querySelector('.msg-vazio')
 const myLibrary = [];
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
-let pVazio = document.createElement('p');
-function displayMsgVazia() {
-    pVazio.innerHTML = 'Biblioteca Vazia...<br>Adicione os livros que leu ou deseja ler';
-    pVazio.setAttribute('class', 'msg-vazio')
-    containerAddLivro.insertBefore(pVazio, containerAddLivro.childNodes[2]);
+function isEmpty() {
+    myLibrary.length === 0 ? pVazio.style.display = "block" : pVazio.style.display = "none"
 }
 
 btnAddNovoLivro.addEventListener('click', () => {
     btnAddNovoLivro.style.display = "none";
     formulario.style.display = "block";
     document.getElementById('titulo-mbiblioteca').style.display = "none";
-    pVazio.style.display = "none";
     livros.style.display = "none";
+    pVazio.style.display = "none";
 });
 
 btnFechar.addEventListener('click', () => {
@@ -38,16 +36,26 @@ btnFechar.addEventListener('click', () => {
     formulario.style.display = "none";
     document.getElementById('titulo-mbiblioteca').style.display = "block";
     livros.style.display = "flex";
+    isEmpty();
 });
 
 btnAdicionar.addEventListener('click', () => {
     let vTitulo = inputTitulo.value;
     let vCapa = inputCapa.value;
+    if (vCapa === "") { vCapa = "./placeholder.png" }
     let vSinopse = inputSinopse.value;
     let vPaginas = inputPaginas.value;
     let vAno = inputAno.value;
     let vLido = inputLido.value;
-    if (!vTitulo || !vCapa || !vSinopse || !vPaginas || !vAno) {
+    if (inputLido.value === "sim") {
+        vLido = "Já li este livro";
+    } else if (inputLido.value === "nao") {
+        vLido = "Não li ainda";
+    } else {
+        vLido = "Estou lendo";
+    }
+
+    if (!vTitulo || !vSinopse || !vPaginas || !vAno) {
         console.log("Preenchimento incorreto")
     } else {
         document.getElementById('titulo-mbiblioteca').style.display = "block";
@@ -86,47 +94,116 @@ function criarCard(lista) {
         li.setAttribute("class", "card")
 
         let h1Titulo = document.createElement('h1');
+        h1Titulo.setAttribute('class', 'titulo-livro')
         h1Titulo.innerText = lista[i].titulo;
 
         let imgCapa = document.createElement('img');
         imgCapa.setAttribute('src', `${lista[i].capa}`);
+        let divImg = document.createElement('div');
+        divImg.setAttribute('class', 'container-img');
+        divImg.appendChild(imgCapa);
 
         let pSinopse = document.createElement('p');
         pSinopse.innerText = lista[i].sinopse;
 
         let smallPagina = document.createElement('small');
-        smallPagina.innerText = 'Páginas: ' + lista[i].paginas;
+        smallPagina.innerHTML = 'Páginas: ' + lista[i].paginas + '<br>';
 
         let smallAno = document.createElement('small');
-        smallAno.innerText = 'Ano: ' + lista[i].ano;
+        smallAno.innerHTML = 'Ano: ' + lista[i].ano + '<br><br>';
 
-        let pLido = document.createElement('p');
-        pLido.innerText = `${lista[i].lido == "sim" ? "Já Lido" : "A ler"}`
+        let btnLido = document.createElement('button');
+        btnLido.setAttribute('class', 'btn-status-leitura');
+        btnLido.innerText = `${lista[i].lido}`
+        if(btnLido.innerText === "Estou lendo") {
+            btnLido.classList.add('lendo')
+            btnLido.classList.remove('nao-lido')
+            btnLido.classList.remove('lido')
+        } else if (btnLido.innerText === "Não li ainda") {
+            btnLido.classList.add('nao-lido')
+            btnLido.classList.remove('lido')
+            btnLido.classList.remove('lendo')
+        } else {
+            btnLido.classList.add('lido')
+            btnLido.classList.remove('nao-lido')
+            btnLido.classList.remove('lendo')
+        }
+        btnLido.addEventListener('click', () => {
+            if(btnLido.classList[1] === 'lido') {
+                btnLido.innerText = "Não li ainda"
+                myLibrary[i].lido = btnLido.innerText
+                btnLido.classList.remove('lido')
+                btnLido.classList.add('nao-lido')
+                salvarLivros();
+            } else if (btnLido.classList[1] === 'nao-lido') {
+                btnLido.innerText = "Estou lendo"
+                myLibrary[i].lido = btnLido.innerText
+                btnLido.classList.remove('nao-lido')
+                btnLido.classList.add('lendo')
+                salvarLivros();
+            } else {
+                btnLido.innerText = "Já li este livro"
+                myLibrary[i].lido = btnLido.innerText
+                btnLido.classList.remove('lendo')
+                btnLido.classList.add('lido')
+                salvarLivros();
+            }
 
-        let br = document.createElement('br');
-
+        });
+        
+        let btnDel = document.createElement('button');
+        btnDel.setAttribute('class', 'btn-del');
+        btnDel.innerHTML = `<svg height="20" width="20" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            viewBox="0 0 512 512"  xml:space="preserve">
+                        <style type="text/css">
+                            .st0{fill:#959595;}
+                        </style>
+                        <g>
+                            <path class="st0" d="M88.594,464.731C90.958,491.486,113.368,512,140.234,512h231.523c26.858,0,49.276-20.514,51.641-47.269
+                                l25.642-335.928H62.952L88.594,464.731z M420.847,154.93l-23.474,307.496c-1.182,13.37-12.195,23.448-25.616,23.448H140.234
+                                c-13.42,0-24.434-10.078-25.591-23.132L91.145,154.93H420.847z"/>
+                            <path class="st0" d="M182.954,435.339c5.877-0.349,10.35-5.4,9.992-11.269l-10.137-202.234c-0.358-5.876-5.401-10.349-11.278-9.992
+                                c-5.877,0.357-10.35,5.409-9.993,11.277l10.137,202.234C172.033,431.231,177.085,435.696,182.954,435.339z"/>
+                            <path class="st0" d="M256,435.364c5.885,0,10.656-4.763,10.656-10.648V222.474c0-5.885-4.771-10.648-10.656-10.648
+                                c-5.885,0-10.657,4.763-10.657,10.648v202.242C245.344,430.601,250.115,435.364,256,435.364z"/>
+                            <path class="st0" d="M329.046,435.339c5.878,0.357,10.921-4.108,11.278-9.984l10.129-202.234c0.348-5.868-4.116-10.92-9.993-11.277
+                                c-5.877-0.357-10.92,4.116-11.277,9.992L319.054,424.07C318.697,429.938,323.17,434.99,329.046,435.339z"/>
+                            <path class="st0" d="M439.115,64.517c0,0-34.078-5.664-43.34-8.479c-8.301-2.526-80.795-13.566-80.795-13.566l-2.722-19.297
+                                C310.388,9.857,299.484,0,286.642,0h-30.651H225.34c-12.825,0-23.728,9.857-25.616,23.175l-2.721,19.297
+                                c0,0-72.469,11.039-80.778,13.566c-9.261,2.815-43.357,8.479-43.357,8.479C62.544,67.365,55.332,77.172,55.332,88.38v21.926h200.66
+                                h200.676V88.38C456.668,77.172,449.456,67.365,439.115,64.517z M276.318,38.824h-40.636c-3.606,0-6.532-2.925-6.532-6.532
+                                s2.926-6.532,6.532-6.532h40.636c3.606,0,6.532,2.925,6.532,6.532S279.924,38.824,276.318,38.824z"/>
+                        </g>
+                        </svg>`;
+                
+        li.appendChild(btnDel); 
         li.appendChild(h1Titulo);
-        li.appendChild(imgCapa);
+        li.appendChild(divImg);
         li.appendChild(pSinopse);
         li.appendChild(smallPagina);
-        li.appendChild(br);
         li.appendChild(smallAno);
-        li.appendChild(pLido);
+        li.appendChild(btnLido);
         livros.appendChild(li);
 
+        btnDel.addEventListener('click', () => {
+            livros.removeChild(li);
+            myLibrary.splice(i, 1);
+            salvarLivros();
+            isEmpty();
+        });     
         // console.log(lista[i]);
     }
 }
 
-let listaDeLivrosSalvos = [];
-let listaJSON = [];
+var listaDeLivrosSalvos = [];
+var listaJSON = [];
 function salvarLivros() {
     listaJSON = JSON.stringify(myLibrary);
     let localSet = localStorage.setItem('livros', listaJSON);
     listaDeLivrosSalvos.push(localSet);
 }
 
-let listaJSONconv = [];
+var listaJSONconv = [];
 function exibirLivrosSalvos() {
     let localGet = localStorage.getItem('livros');
     listaJSONconv = JSON.parse(localGet);
@@ -141,8 +218,4 @@ if (localStorage.getItem('livros')) {
     exibirLivrosSalvos();
 }
 
-
-if (myLibrary.length === 0) {
-    displayMsgVazia();
-}
-
+isEmpty();
